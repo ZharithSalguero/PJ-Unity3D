eb69f47b7f6e2c830b4d95c786ed215c65b78146
Zharith Salguero <458616.clot@fje.edu>
2025-04-25 11:46:24 +0200

    Fixing UI Menu

---
+++ Assets/Script/ButtonAudio.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonSound : MonoBehaviour, IPointerEnterHandler
+{
+    public AudioClip clip;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("The cursor entered the selectable UI element.");
+        AudioManager.INSTANCE.source.PlayOneShot(clip);
+    }
+}
